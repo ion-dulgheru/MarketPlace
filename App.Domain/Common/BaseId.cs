94b1fd8ed81410ca49052dd83f319fae57265227
Pavlov Iulian <iulian.pavlov@isa.utm.md>
2026-09-09 14:02:38 +0300

    din get am facut set

--- App.Domain/Common/BaseId.cs
+++ App.Domain/Common/BaseId.cs
@@ -2,11 +2,11 @@ namespace App.Domain.Common
 
 public abstract class BaseEntity {
     
-    public long Id {get; protected get; }
-    public guid Uuid {get; protected get; } Guid.NewGuid();
-    public bool IsActive {get; protected get;} = true;
-    public DateTime CreatedAt {get; protected get; } = DateTime.UtcNow;
-    public DateTime? UpdateAt {get; protected get;}
+    public long Id {get; protected set; }
+    public guid Uuid {get; protected set; } Guid.NewGuid();
+    public bool IsActive {get; protected set;} = true;
+    public DateTime CreatedAt {get; protected set; } = DateTime.UtcNow;
+    public DateTime? UpdateAt {get; protected set;}
     
 }
 
