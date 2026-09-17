b989b5552a0be1734af4effe35517c258381a9dd
vitalieww <vitalie.vasilean@isa.utm.md>
2026-09-17 15:04:02 +0300

    add uuid response to photos

--- backend/App.Application/UseCases/Adverts/GetMyAdvert/GetMyAdvertsCommandHandler.cs
+++ backend/App.Application/UseCases/Adverts/GetMyAdvert/GetMyAdvertsCommandHandler.cs
@@ -39,7 +39,7 @@ public class GetMyAdvertsCommandHandler(IAdvertRepository advertRepository)
                 advert.Photos
                     .OrderByDescending(p => p.IsPrimary)
                     .Take(1)
-                    .Select(p => new AdvertPhotoResponse(p.PhotoUrl, p.IsPrimary))
+                    .Select(p => new AdvertPhotoResponse(p.Guid, p.PhotoUrl, p.IsPrimary))
                     .ToList()))
             .ToList();
 
--- backend/App.Contracts/Responses/Adverts/AdvertPhotoResponse.cs
+++ backend/App.Contracts/Responses/Adverts/AdvertPhotoResponse.cs
@@ -1,6 +1,7 @@
 namespace App.Contracts.Responses.Adverts;
 
 public record AdvertPhotoResponse(
+    Guid Uuid,
     string PhotoUrl,
     bool IsPrimary
 );
