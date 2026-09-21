9b824d54aea717bf400b5517fe1e5aa2a972dce7
id18-v <ion.dulgheru@isa.utm.md>
2026-09-21 15:52:54 +0300

    feat(application): add ChangePasswordCommand and validation rules

---
+++ backend/App.Application/UseCases/Users/ChangePassword/ChangePasswordCommandValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+
+namespace App.Application.UseCases.Users.ChangePassword;
+
+public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+{
+    public ChangePasswordCommandValidator()
+    {
+        RuleFor(x => x.CurrentPassword).NotEmpty();
+        RuleFor(x => x.NewPassword).NotEmpty().MinimumLength(8);
+    }
+}
